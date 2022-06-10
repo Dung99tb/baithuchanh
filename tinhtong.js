8d31f23fdbcf29f003c82be3f04d0d7eac9b3fa8
pndungtb@gmail.com <pndungtb@gmail.com>
2022-06-10 08:41:27 +0700

    Add file tinhtong.js

---
+++ tinhtong.js
@@ -0,0 +1,14 @@
+function tinhtong() {
+    var sum1=0, sum2=0;
+    for ( i=1; i<=100; i++) {
+        if ( i%2==0 ){
+            sum1 +=i ;
+        } else {
+            sum2 += i;
+        }
+    }
+    document.writeln("Có tổng số chẵn là: ",sum1);
+    document.writeln("<br/>");
+    document.writeln("Có tổng số lẻ là: ",sum2);
+}
+tinhtong();
